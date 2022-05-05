4cf589f711ce3fce36a140a9d727b16715c14f4d
XuYang5478 <1826712273@qq.com>
2022-05-05 17:29:47 +0800

    增加持久化函数

---
+++ CounterState/index.js
@@ -0,0 +1,27 @@
+﻿/*
+ * This function is not intended to be invoked directly. Instead it will be
+ * triggered by an orchestrator function.
+ * 
+ * Before running this sample, please:
+ * - create a Durable orchestration function
+ * - create a Durable HTTP starter function
+ * - run 'npm install durable-functions' from the wwwroot folder of your
+ *   function app in Kudu
+ */
+
+const df = require("durable-functions")
+
+module.exports = df.entity(function (context) {
+    const currentValue = context.df.getState(() => 0);
+    switch (context.df.operationName) {
+        case "add":
+            context.df.setState(currentValue + 1);
+            break;
+        case "get":
+            context.df.return(currentValue);
+            break;
+        case "reset":
+            context.df.setState(0);
+            break;
+    }
+});
